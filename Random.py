7dae0e22989213a890d1add7cbb0de9d9f56976e
p3rso <88558386+p3rso@users.noreply.github.com>
2022-07-13 22:43:09 -0400

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,25 @@
+def GenNumb(Rinp,floor,ceil):
+    aout = Rinp/(Rinp**0.5)
+    bout = aout*10000000
+    cout = aout*100000
+    cout = int(cout)
+    cout = cout*100
+    dout = bout - cout
+    eout = Rinp+1/((Rinp)**0.5)
+    fout = eout*10000000
+    gout = eout*100000
+    gout = int(gout)
+    gout = gout*100
+    hout = fout - gout
+    dout = dout-10
+    hout = hout-10
+    hout = hout*(1/0.9)
+    dout = dout*(1/0.9)
+    if dout != 0:
+        dout=(((ceil-floor)/100)*dout)
+        dout=dout+(2*floor)
+        return(int(dout))
+    else:
+        hout=(((ceil-floor)/100)*hout)
+        hout=hout+(2*floor)
+        return(int(hout))
